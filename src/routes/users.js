a792511cb066c0d1dbcaab11519235eba218ea4d
TrpFrog <dev@trpfrog.net>
2024-03-30 12:25:47 +0900

    add ensureAuthenticated

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,9 @@
 const { Hono } = require("hono");
+const { ensureAuthenticated } = require("../middlewares");
 
 const app = new Hono();
+
+app.use(ensureAuthenticated());
 
 app.get("/", (c) => {
   return c.text("respond with a resource");
